05288b4e3f2d4c5c84131be9e9854c5cf1d2272e
Roberto-Paul Malutan <roberto-paul.malutan@student.manchester.ac.uk>
2015-03-01 14:28:23 +0000

    adding the second menu bar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu').prepend('<div id="bg-one"></div><div id="bg-two"></div><div id="bg-three"></div><div id="bg-four"></div>');
+});
+} )( jQuery );
+
