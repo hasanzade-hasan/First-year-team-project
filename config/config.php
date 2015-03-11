28777d27edba09bb708bb1226b1da5777391cad5
[DONG EUN SEO] <dong.seo@student.manchester.ac.uk>
2015-03-11 15:34:34 +0000

    6th

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
-<?
+<?php
 header("Content-Type: text/html;charset=utf-8");
 session_start();
 extract($_POST);
--- config/connect.php
+++ config/connect.php
@@ -1,4 +1,4 @@
-<?
+<?php
   mysql_connect('dbhost.cs.man.ac.uk', 'mbax4cb3', 'BubblesBubbles') or die( "Unable to connect to SQL server");
   mysql_select_db("mbax4cb3") or die( "Unable to select database");
   mysql_query("SET sql_mode = 'HIGH_NOT_PRECEDENCE';");
