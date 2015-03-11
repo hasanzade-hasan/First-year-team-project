557a3855ad1bcd2219fed017eb6576a9bba51ecf
[DONG EUN SEO] <dong.seo@student.manchester.ac.uk>
2015-03-11 15:42:35 +0000

    7th?

--- config/config.php
+++ config/config.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
 session_start();
+ini_set("display_errors", 1);
 extract($_POST);
 extract($_GET);
 $admin_id 			= "sanoadmin";						
--- config/connect.php
+++ config/connect.php
@@ -1,5 +1,5 @@
 <?php
-  mysql_connect('dbhost.cs.man.ac.uk', 'mbax4cb3', 'BubblesBubbles') or die( "Unable to connect to SQL server");
+  mysqli_connect('dbhost.cs.man.ac.uk', 'mbax4cb3', 'BubblesBubbles') or die( "Unable to connect to SQL server");
   mysql_select_db("mbax4cb3") or die( "Unable to select database");
   mysql_query("SET sql_mode = 'HIGH_NOT_PRECEDENCE';");
 ?>
