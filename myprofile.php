8f3d9cdf599068f962f130362cd1ea57875e5a0f
Roberto-Paul Malutan <roberto-paul.malutan@student.manchester.ac.uk>
2015-03-17 16:58:25 +0000

    design

--- myprofile.php
+++ myprofile.php
@@ -78,6 +78,24 @@
 
 						</div>
 
+
+						<div id="box2">
+							<h1> exercise of the day </h1>									
+							<?php
+								$row=getdata(	"select * from ExercisesTable order by rand() limit 0 , 1 " , $conn);
+								$Photo = "";
+								$media_height=210;
+								$media_width=354;
+
+								if( $row["Photo"] != "") { 
+										$Photo = "<iframe width=\"$media_width\" height=\"$media_height\" src=\"" . $row["Photo"] ."\" frameborder=\"0\" allowfullscreen></iframe>"; 
+								}
+								echo $row["ExerciseName"] . "<br/ >";
+								echo $Photo . "<br/ >";
+								echo substr( $row["Description"] , 0 , 50 );
+							?>
+						</div>
+						
 						<div id="box3">
 								<h1> Your meals for today </h1>
 								<ul>
@@ -114,22 +132,6 @@
 							</ul>
 
 							</div>
-						<div id="box2">
-							<h1> exercise of the day </h1>									
-							<?php
-								$row=getdata(	"select * from ExercisesTable order by rand() limit 0 , 1 " , $conn);
-								$Photo = "";
-								$media_height=210;
-								$media_width=354;
-
-								if( $row["Photo"] != "") { 
-										$Photo = "<iframe width=\"$media_width\" height=\"$media_height\" src=\"" . $row["Photo"] ."\" frameborder=\"0\" allowfullscreen></iframe>"; 
-								}
-								echo $row["ExerciseName"] . "<br/ >";
-								echo $Photo . "<br/ >";
-								echo substr( $row["Description"] , 0 , 50 );
-							?>
-						</div>
 						
 							
 							<div id="box2">
