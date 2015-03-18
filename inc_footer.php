f8eabea310b56059d021156901f79a39100bb14e
[DONG EUN SEO] <dong.seo@student.manchester.ac.uk>
2015-03-18 17:10:54 +0000

    logout

--- inc_footer.php
+++ inc_footer.php
@@ -1,7 +1,12 @@
 
-			<div id = "logout">
+		<?php
+                     if ( isset($_SESSION["sn_idx"]) ) {?>
+	
+                        <div id = "logout">
 				<span class="button small" onclick="javascript:location.href='member.php?mode=logout';"> Log out</span>
 			</div>
+                <?php
+                        }?>
 			<footer id="footer">
 				<div class="container">
 					<ul class="copyright">
