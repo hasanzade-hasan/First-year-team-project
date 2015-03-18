43fa60558d7529d31365295603afbdd9156810e6
[DONG EUN SEO] <dong.seo@student.manchester.ac.uk>
2015-03-18 21:32:52 +0000

    cal

--- calculator.php
+++ calculator.php
@@ -16,6 +16,11 @@
 		}else {
 			$mode_frm = "add";
 		}
+
+		if($sdate != ""){
+		   	$sql = "select datediff(".$sdate.","."curdate())"
+			$res = mysqli_query( $conn, $sql) OR die(__FILE__." : Line ".__LINE__."<p>".mysql_error());
+		}
 ?>
 <script src="js/ajax.js"></script>
 <style type="text/css" title="">
