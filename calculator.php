abe30b48f972936871bc4c3e6e68cb96cefb26f0
[DONG EUN SEO] <dong.seo@student.manchester.ac.uk>
2015-03-18 23:30:54 +0000

    calculator2

--- calculator.php
+++ calculator.php
@@ -148,7 +148,7 @@
 
 			function addFood(val) {
 				document.getElementById("Food_list_"+val).checked=true;
-				document.getElementById("Food_row_"+val).style.display="aaaaaaaaaaa";
+				document.getElementById("Food_row_"+val).style.display="1";
 				cal(val);
 			}
 
