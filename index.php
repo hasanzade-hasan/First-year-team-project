35d9bd6aafc0b3edd89fc798fe15a392ef81b5c1
Hasan Hasanzade <hasan.hasanzade@student.manchester.ac.uk>
2015-03-19 17:05:58 +0000

    changed text on a button to "Go to MyProfile"

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ include "./config/query.php";
 					<ul class="actions">
 					<?php 
 					if ( isset($_SESSION["sn_idx"]) ) {?>
-						<li><span href="#" class="button big special" id="toggle-login" onclick="javascript:location.href='./myprofile.php';">Go Myprofile</span></li>
+						<li><span href="#" class="button big special" id="toggle-login" onclick="javascript:location.href='./myprofile.php';">Go to Myprofile</span></li>
 					<?php
 					} else {?>
 						<li><span href="#" class="button big special" id="toggle-login">Log in</span> <div id="login">
