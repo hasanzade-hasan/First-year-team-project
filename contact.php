12142d8411a19546ee5a4d0cfcc182acb5d28f99
Colm Mallon <colm.mallon@student.manchester.ac.uk>
2015-03-19 19:16:59 +0000

    Colms contact para done

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@
 						
 						<img src="images/salad.jpg" width ="100%">
 						<h3>Colm Mallom </h3>
-						<p>Nullam quis eleifend lectus. Suspendisse et lacus placerat, suscipit erat id, ullamcorper nisl. Maecenas gravida dictum fringilla. Phasellus lobortis, felis id accumsan condimentum, magna erat malesuada ligula, quis egestas urna nisl nec velit. Etiam at metus sit amet ex feugiat tincidunt.</p>
+						<p>Hi my name is Colm Mallon. I am a Computer Science Masters student at the University of Manchester. I am currently a first year on the program. I have 4 years programming experience with Java, python, vb.net, ARM and PASCAL programming languages all included.</p>
 					</div>
 					<div id="box">
 						<img src="images/work.jpg" width ="100%">
