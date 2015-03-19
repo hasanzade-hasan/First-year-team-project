b5d735a293371f5ae494c069b28582c6750e3311
Cristian Brisan <cristian.brisan@student.manchester.ac.uk>
2015-03-19 19:30:00 +0000

    Mein contact information

--- contact.php
+++ contact.php
@@ -10,9 +10,15 @@
 				<div id = "main">
 					<div id="box">
 						
+<<<<<<< HEAD
 						<img src="images/Colm.jpg" width ="100%">
 						<h3>Colm Mallom </h3>
 						<p>Hi my name is Colm Mallon. I am a Computer Science Masters student at the University of Manchester. I am currently a first year on the program. I have 4 years programming experience with Java, python, vb.net, ARM and PASCAL programming languages all included.</p>
+=======
+						<img src="images/salad.jpg" width ="100%">
+						<h3>Colm Mallon </h3>
+						<p>Nullam quis eleifend lectus. Suspendisse et lacus placerat, suscipit erat id, ullamcorper nisl. Maecenas gravida dictum fringilla. Phasellus lobortis, felis id accumsan condimentum, magna erat malesuada ligula, quis egestas urna nisl nec velit. Etiam at metus sit amet ex feugiat tincidunt.</p>
+>>>>>>> Mein contact information
 					</div>
 					<div id="box">
 						<img src="images/work.jpg" width ="100%">
@@ -22,7 +28,7 @@
 					<div id="box">
 						<img src="images/diet.jpg" width ="100%">
 						<h3>Cristian Brisan</h3>
-						<p>Nullam quis eleifend lectus. Suspendisse et lacus placerat, suscipit erat id, ullamcorper nisl. Maecenas gravida dictum fringilla. Phasellus lobortis, felis id accumsan condimentum, magna erat malesuada ligula, quis egestas urna nisl nec velit. Etiam at metus sit amet ex feugiat tincidunt.</p>
+						<p>Hi, I am Cris! I, along with Colm and Dong, am in charge of the backend side of our website. This means that whenever you have any complaints or suggestions about the data we have provided you on foods, diets or exercises it's me you should talk to :). My email is: cristian.brisan@student.manchester.ac.uk </p>
 					</div>
 					<div id="box">
 						
