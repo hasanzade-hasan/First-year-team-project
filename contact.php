95f51b0c3aafe8034268a54f54d7c2c1e120a0e8
Hasan Hasanzade <hasan.hasanzade@student.manchester.ac.uk>
2015-03-20 12:35:40 +0000

    italic

--- contact.php
+++ contact.php
@@ -13,33 +13,33 @@
 
 						<img class="boxImg" src="images/ColmContactPhoto.jpg" width ="100%">
 						<h3>Colm Mallon </h3>
-						<p>Hi my name is Colm Mallon. I am a Computer Science Masters student at the University of Manchester. I am currently a first year on the program. I have 4 years programming experience with Java, python, vb.net, ARM and PASCAL programming languages all included.</p>
+						<p>Hi my name is Colm Mallon. I am a Computer Science Masters student at the University of Manchester. I am currently a first year on the program. I have 4 years programming experience with Java, python, vb.net, ARM and PASCAL programming languages all included.<em>colm.mallon@student.manchester.ac.uk</em></p>
 					</div>
 					<div id="box">
 						<img class="boxImg" src="images/RobertoContactPhoto.jpg" width ="100%">
 						<h3>Roberto-Paul Malutan</h3>
-						<p>Hi, I'm Roberto! I'm a student at the UoM's Computer Science School. Alongside with football, programming is my greatest passion, which is the reason I chose to work on the frontend side of the website. Should you have any enquiries do not hesitate to contact me at <br>roberto.malutan@student.manchester.ac.uk <br> or on my phone 07511 481 676 </p>
+						<p>Hi, I'm Roberto! I'm a student at the University of Manchester's Computer Science School. Alongside with football, programming is my greatest passion, which is the reason I chose to work on the frontend side of the website. Should you have any enquiries do not hesitate to contact me at <br><em>roberto.malutan@student.manchester.ac.uk</em> <br> or on my phone 07511 481 676 </p>
 						</div>
 					<div id="box">
 						<img class="boxImg" src="images/CrisContactPhoto.jpg" width ="100%">
 						<h3>Cristian Brisan</h3>
-						<p>Hi, I am Cris! I, along with Colm and Dong, am in charge of the backend side of our website. This means that whenever you have any complaints or suggestions about the data we have provided you on foods, diets or exercises it's me you should talk to :). <br>My email is: cristian.brisan@student.manchester.ac.uk </p>
+						<p>Hi, I am Cris! I, along with Colm and Dong, am in charge of the backend side of our website. This means that whenever you have any complaints or suggestions about the data we have provided you on foods, diets or exercises it's me you should talk to :). <br>My email is: <em>cristian.brisan@student.manchester.ac.uk </em></p>
 					</div>
 					<div id="box">
 						
 						<img class="boxImg" src="images/Hasan.jpg" width ="100%">
 						<h3>Hasan Hasanzade </h3>
-						<p>Hello, I am Hasan. I was working on this project with other five guys you see here. I did front-end part of this website together with Roberto and Ulrich. I am not sure this is perfect design, if you have any suggestions email me here: <i>hasan.hasanzade@student.manchester.ac.uk</i></p>
+						<p>Hello, I am Hasan. I was working on this project with other five guys you see here. I did front-end part of this website together with Roberto and Ulrich. I am not sure this is perfect design, if you have any suggestions email me here: <em>hasan.hasanzade@student.manchester.ac.uk</em></p>
 					</div>
 					<div id="box">
 						<img class="boxImg" src="images/Eun.jpg" width ="100%">
 						<h3>Dongeun Seo </h3>
-						<p>Hello, I am Dongeun Seo. I have managed PHP and some other languages on this website. Whenever you find an error with regard to functionality, contact me please.<br>dong.seo@student.manchester.ac.uk</p>
+						<p>Hello, I am Dongeun Seo. I have managed PHP and some other languages on this website. Whenever you find an error with regard to functionality, contact me please.<br><em>dong.seo@student.manchester.ac.uk</em></p>
 						</div>
 					<div id="box">
 						<img class="boxImg" src="images/ulrichContactPhoto.jpg" width ="100%">
 						<h3>Ulrich Balk</h3>
-						<p>Hello, I am Ulrich, I worked on the frontend together with Roberto and Hasan. So if you find any visual glitches and the like, contact me at: ulrich.balk@student.manchester.ac.uk</p>
+						<p>Hello, I am Ulrich, I worked on the frontend together with Roberto and Hasan. So if you find any visual glitches and the like, contact me at: <em>ulrich.balk@student.manchester.ac.uk</em></p>
 					</div>
 		
 				</div>
