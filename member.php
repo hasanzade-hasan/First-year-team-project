dde5d8bbbda267813e17145780b78651e3922656
[DONG EUN SEO] <dong.seo@student.manchester.ac.uk>
2015-03-20 10:30:33 +0000

    update

--- member.php
+++ member.php
@@ -50,6 +50,6 @@ if ($mode == "regist" ) {
 	}
 }elseif ($mode == "logout" ) {
 	session_unset();
-	echo "<script>location.href = '/';</script>";
+	echo "<script>location.href = './index.php';</script>";
 }
 ?>
